919d0b4b8e9682f4bf6b40a0034b781d0772543c
UmidjonOkhunov <umidjon@kaist.ac.kr>
2020-08-04 17:19:17 +0900

    push frontend build for heroku deployment

---
+++ build/precache-manifest.5df0d16c0d633e2cae16b1d14dbe3ec0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f5dcb63a34f6244ae14ec0f83b9b0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbac16f1c4590ab90ddf",
+    "url": "/static/js/2.9a96945a.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/js/2.9a96945a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "944f4da61df253b693b8",
+    "url": "/static/js/main.e4a8e3cc.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
